ba277062a03331b48c80eb516b2f64968302e00b
cantalupo555 <cantalupo@protonmail.com>
2018-08-01 17:40:43 -0300

    Update

---
+++ qbittorrent-backup-linux.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ $UID -ne 0 ]; then
+    echo "Install failed: you must be logged in as 'root' to install."
+    echo "Use command 'sudo -i', then enter root password and then try again."
+    exit 1
+fi
