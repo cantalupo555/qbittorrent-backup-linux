ffe87340831792194d324759dddf404a51bfa940
cantalupo555 <cantalupo@protonmail.com>
2018-08-01 17:44:55 -0300

    Update

--- qbittorrent-backup-linux.sh
+++ qbittorrent-backup-linux.sh
@@ -4,3 +4,8 @@ if [ $UID -ne 0 ]; then
     echo "Use command 'sudo -i', then enter root password and then try again."
     exit 1
 fi
+cd ~
+mkdir qBittorrent/
+mkdir 
+cp -R ~/.config/qBittorrent/* ~/qBittorrent
+cp~/.local/share/data/qBittorrent/BT_backup/
