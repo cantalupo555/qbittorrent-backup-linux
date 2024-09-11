c7364fe4398d20c25c75f03c5ca817d57a95a1a4
cantalupo555 <cantalupo@protonmail.com>
2024-09-10 21:50:32 -0300

    Added support for qBittorrent Flatpak client
    Improve backup and restore logic
    Enhance qBittorrent detection
    Add detection for qBittorrent Flatpak installations
    Handle multiple qBittorrent installations
    Prompt user to choose an installation when multiple are found
    Improve error handling and messaging throughout the script
    Improvements to existing comments

--- qbittorrent-backup-linux.sh
+++ qbittorrent-backup-linux.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# cantalupo555/vdbhb59
-
-# Check requirements
-# "comment out with # or delete" the locate package which your OS does not need. Example PopOS (Debian) although has mlocate installed, but still needs locate to work.
+# Check dependencies and user permissions
 if [ $UID -ne 0 ]; then
     echo -e "\n\033[1;31mRun failed. Try again using:\033[0m"
     echo "sudo ./qbittorrent-backup-linux.sh"
@@ -20,8 +17,10 @@ elif [ ! -f /usr/bin/plocate ]; then
     echo -e "\n\033[1;31mPlease install the 'plocate' package.\033[0m"
     echo "sudo apt install plocate"
     exit 1
-elif [ ! -f /usr/bin/qbittorrent ]; then
+elif [ ! -f /usr/bin/qbittorrent ] && ( [ -z "$(which flatpak)" ] || [ -z "$(flatpak list | grep org.qbittorrent.qBittorrent)" ] ); then
+    # Checks if qBittorrent is installed
     echo -e "\n\033[1;31mqBittorrent installation not found.\033[0m"
+    echo "Please install qBittorrent via package manager or Flatpak."
     exit 1
 fi
 
@@ -34,6 +33,82 @@ o1() {
     echo "Press ENTER to go back!"
     echo ""
     read v
+}
+
+detect_qbittorrent_config() {
+    # Detect qBittorrent configuration directory
+    echo -e "Please wait.\nDetecting configuration qBittorrent client..."
+    sleep 2
+    echo -e "\n\033[1;31mThis may take a few seconds.\033[0m"
+    # Update the locate database to ensure the latest file locations are known
+    sudo updatedb
+    sleep 2
+
+    clear
+    # Search for the qBittorrent configuration directory using plocate
+    # Store plocate output in array to handle multiple installations
+    mapfile -t dirCheck < <(plocate *config/qBittorrent/qBittorrent.conf)
+    # Check if the configuration directory was found
+    if [ ${#dirCheck[@]} -eq 0 ]; then
+        # If not found, display an error message and exit
+        echo -e "\033[1;31mqBittorrent configuration not found, launch qBittorrent client before running this script.\033[0m\n"
+        exit 1
+    else
+        # Handle multiple installations
+        if [ ${#dirCheck[@]} -eq 1 ]; then
+            # If only one installation is found, use its directory and user
+            dir=$(echo "${dirCheck[0]}" | sed 's|\(/home/[^/]*\)/.*|\1/|')
+            user=$(echo "$dir" | cut -d'/' -f3)
+            # Check if the installation is a Flatpak
+            if echo "${dirCheck[0]}" | grep -q "org.qbittorrent.qBittorrent"; then
+                is_flatpak=true
+                dirFlatpak=$(echo "${dirCheck[0]}" | sed 's|\(.*org\.qbittorrent\.qBittorrent\).*|\1/|')
+            else
+                is_flatpak=false
+            fi
+        else
+            # Let user choose from multiple qBittorrent installations
+            echo -e "\033[1;32mFound multiple qBittorrent installations:\033[0m\n"
+            for i in "${!dirCheck[@]}"; do
+                if echo "${dirCheck[$i]}" | grep -q "org.qbittorrent.qBittorrent"; then
+                    echo -e "\033[38;5;63m$((i+1)) - [Flatpak]\033[0m ${dirCheck[$i]}"
+                else
+                    echo -e "\033[1;36m$((i+1)) - [Default]\033[0m ${dirCheck[$i]}"
+                fi
+            done
+            echo -e "\033[1;31m3 - Exit\033[0m"
+
+            # Ask the user to choose an installation
+            echo -e "\n\033[1;33mWhich installation do you want to use? (Input the number: 1, 2 or 3)\033[0m"
+            read -r choice
+
+            # Handle the user's choice
+            if [ "$choice" -eq 3 ]; then
+                clear
+                echo "You selected option 3."
+                echo "Exiting..."
+                exit 1
+            elif [ "$choice" -le "${#dirCheck[@]}" ] && [ "$choice" -gt 0 ]; then
+                selected_config="${dirCheck[$((choice-1))]}"
+                # Use its directory and user based on the choice
+                dir=$(echo "$selected_config" | sed 's|\(/home/[^/]*\)/.*|\1/|')
+                user=$(echo "$dir" | cut -d'/' -f3)
+                # Check if the installation is a Flatpak
+                if echo "$selected_config" | grep -q "org.qbittorrent.qBittorrent"; then
+                    is_flatpak=true
+                    dirFlatpak=$(echo "$selected_config" | sed 's|\(.*org\.qbittorrent\.qBittorrent\).*|\1/|')
+                else
+                    is_flatpak=false
+                fi
+            else
+                clear
+                echo "Invalid choice."
+                echo "You need to choose between 1 and 3."
+                echo "Exiting..."
+                exit 1
+            fi
+        fi
+    fi
 }
 
 # Switch case
@@ -54,21 +129,29 @@ do
         1) while true; do
             clear
 
-            # Check config directory
-            sudo updatedb
-            dirCheck=$(plocate *config/qBittorrent/qBittorrent.conf)
-            if [ -z $dirCheck ]; then
-                echo -e "\033[1;31mqBittorrent configuration not found, launch qBittorrent client before running this script.\033[0m\n"
-                exit 1
-            else
-                # Set user and home directory without $UID, $USER or $HOME tag
-                plocate *config/qBittorrent/qBittorrent.conf > dirUser
-                dir=$(cut -d"." -f1 dirUser)
-                user=$(cut -d"/" -f3 dirUser)
-                rm dirUser
-            fi
+            # Call the function to detect the qBittorrent configuration directory and user
+            detect_qbittorrent_config
 
-            # Shutdown qbittorrent
+            # View configuration details
+            clear
+            echo "Selected directory: $dir"
+            echo "Selected user: $user"
+            if [ "$is_flatpak" = true ]; then
+                # If it's a Flatpak installation, display the corresponding directory
+                echo -e "Flatpak directory: ${dirFlatpak}\n"
+            fi
+            sleep 3
+
+            # Display a message during backup initialization
+            cont=1
+            while [ $cont -ne 4 ]; do
+                echo -e "\033[1;33mStarting backup...\033[0m\n"
+                sleep 1
+                ((cont=$cont+1))
+            done
+            sleep 3
+
+            # Stop the qBittorrent client to prevent conflicts during backup
             killall qbittorrent
             clear
             cont=1
@@ -79,28 +162,35 @@ do
             done
             sleep 5
 
-            # Create .zip structure to save the data
+            # Create directory structure for backup
             cd $dir
-            rm -f "$dir"qBittorrent-Backup-Linux.zip
+            rm -f ${dir}qBittorrent-Backup-Linux.zip
+            rm -rf ${dir}qBittorrent-Backup-Linux/
             mkdir qBittorrent-Backup-Linux/
             mkdir qBittorrent-Backup-Linux/qBittorrent/
             mkdir qBittorrent-Backup-Linux/BT_backup/
             mkdir qBittorrent-Backup-Linux/logs/
 
-            # Saving the data
+            # Save configuration data to backup
             # Directory change after version 4.2.5
             # "LINUX: Don't create 'data' subdirectory in XDG_DATA_HOME (lbilli)"
             # https://www.qbittorrent.org/news.php
-            cp -R $dir.config/qBittorrent/* qBittorrent-Backup-Linux/qBittorrent/
-            if [ -d $dir.local/share/qBittorrent/ ]; then
-                cp -R $dir.local/share/qBittorrent/BT_backup/* qBittorrent-Backup-Linux/BT_backup/
-                cp -R $dir.local/share/qBittorrent/logs/* qBittorrent-Backup-Linux/logs/
+            if [ "$is_flatpak" = true ]; then
+                cp -R ${dirFlatpak}config/qBittorrent/* qBittorrent-Backup-Linux/qBittorrent/
+                cp -R ${dirFlatpak}data/qBittorrent/BT_backup/* qBittorrent-Backup-Linux/BT_backup/
+                cp -R ${dirFlatpak}data/qBittorrent/logs/* qBittorrent-Backup-Linux/logs/
             else
-                cp -R $dir.local/share/data/qBittorrent/BT_backup/* qBittorrent-Backup-Linux/BT_backup/
-                cp -R $dir.local/share/data/qBittorrent/logs/* qBittorrent-Backup-Linux/logs/
+                cp -R ${dir}.config/qBittorrent/* qBittorrent-Backup-Linux/qBittorrent/
+                if [ -d ${dir}.local/share/qBittorrent/ ]; then
+                    cp -R ${dir}.local/share/qBittorrent/BT_backup/* qBittorrent-Backup-Linux/BT_backup/
+                    cp -R ${dir}.local/share/qBittorrent/logs/* qBittorrent-Backup-Linux/logs/
+                else
+                    cp -R ${dir}.local/share/data/qBittorrent/BT_backup/* qBittorrent-Backup-Linux/BT_backup/
+                    cp -R ${dir}.local/share/data/qBittorrent/logs/* qBittorrent-Backup-Linux/logs/
+                fi
             fi
 
-            # Last Validation
+            # Checks if the configuration file was copied correctly to the backup
             if [ ! -f "$dir"qBittorrent-Backup-Linux/qBittorrent/qBittorrent.conf ]; then
                 clear
                 echo -e "\033[1;31mFailed to backup.\033[0m\n"
@@ -110,13 +200,13 @@ do
             zip -r -0 qBittorrent-Backup-Linux.zip qBittorrent-Backup-Linux/
             rm -rf "$dir"qBittorrent-Backup-Linux/
 
-            # Set permission
+            # Set permissions for the backup file
             chown $user:$user qBittorrent-Backup-Linux.zip
             sleep 2
 
-            # End
+            # Displays the backup completion message and restore instructions
             clear
-            echo -e "\n\033[1;32mBackup completed.\033[0m"
+            echo -e "\n\033[1;32mBackup completed.\033[0m\n"
             echo "Backup saved to:"
             echo -e "\e[1m$dir\e[0m""\e[1mqBittorrent-Backup-Linux.zip\e[0m"
             echo -e "\n\n\e[4mHow restore? Have this file anywhere on your system and select option 2.\e[0m"
@@ -131,26 +221,37 @@ do
         2) while true; do
             clear
 
-            # Check config directory
-            sudo updatedb
-            dirCheck=$(plocate *config/qBittorrent/qBittorrent.conf)
-            if [ -z $dirCheck ]; then
-                echo -e "\033[1;31mqBittorrent configuration not found, launch qBittorrent client before running this script.\033[0m\n"
-                exit 1
-            else
-                # Set user and home directory without $UID, $USER or $HOME tag
-                plocate *config/qBittorrent/qBittorrent.conf > dirUser
-                dir=$(cut -d"." -f1 dirUser)
-                user=$(cut -d"/" -f3 dirUser)
-                rm dirUser
-            fi
+            # Call the function to detect the qBittorrent configuration directory and user
+            detect_qbittorrent_config
 
-            # Search qBittorrent-Backup-Linux.zip
+            # View configuration details
+            clear
+            echo "Selected directory: $dir"
+            echo "Selected user: $user"
+            if [ "$is_flatpak" = true ]; then
+                # If it's a Flatpak installation, display the corresponding directory
+                echo -e "Flatpak directory: ${dirFlatpak}\n"
+            fi
+            sleep 3
+
+            # Display a message during restore initialization
+            cont=1
+            while [ $cont -ne 4 ]; do
+                echo -e "\033[1;33mStarting restore...\033[0m\n"
+                sleep 1
+                ((cont=$cont+1))
+            done
+            sleep 3
+
+            # Search for the qBittorrent-Backup-Linux.zip file
+            # Remove any existing backup file in the Trash directory
             rm -f $dir.local/share/Trash/files/qBittorrent-Backup-Linux.zip
             rm -f $dir.local/share/Trash/info/qBittorrent-Backup-Linux.zip.trashinfo
+            # Update the locate database to ensure the latest file locations are known
             sudo updatedb
             bkp=$(plocate qBittorrent-Backup-Linux.zip)
             plocate qBittorrent-Backup-Linux.zip > zipCheck
+            # Count the number of lines in the zipCheck file to determine if there are multiple backup files
             zipFile=$(awk 'END{print NR}' zipCheck)
             if [ $zipFile -gt 1 ]; then
                 clear
@@ -167,7 +268,7 @@ do
             fi
             rm zipCheck
 
-            # Shutdown qbittorrent
+            # Stop the qBittorrent client to prevent conflicts during restore
             killall qbittorrent
             clear
             cont=1
@@ -176,7 +277,7 @@ do
                 sleep 1
                 ((cont=$cont+1))
             done
-            sleep 10
+            sleep 5
 
             # Unzip file
             cd $dir
@@ -184,7 +285,7 @@ do
             unzip $bkp
             chown -R $user:$user qBittorrent-Backup-Linux/
 
-            # Check content
+            # Check if the backup file was extracted correctly
             if [ ! -f "$dir"qBittorrent-Backup-Linux/qBittorrent/qBittorrent.conf ]; then
                 clear
                 echo -e "\033[1;31mFailed to restore.\033[0m\n"
@@ -192,34 +293,46 @@ do
                 exit 1
             fi
 
-            # Restoring qBittorrent
+            # Restore qBittorrent configuration and data
             # Directory change after version 4.2.5
             # "LINUX: Don't create 'data' subdirectory in XDG_DATA_HOME (lbilli)"
             # https://www.qbittorrent.org/news.php
-            rm -rf $dir.config/qBittorrent/*
-            cp -R qBittorrent-Backup-Linux/qBittorrent/* $dir.config/qBittorrent/
-            chown -R $user:$user $dir.config/qBittorrent/
-            if [ -d $dir.local/share/qBittorrent/ ]; then
-                rm -rf $dir.local/share/qBittorrent/BT_backup/*
-                cp -R qBittorrent-Backup-Linux/BT_backup/* $dir.local/share/qBittorrent/BT_backup/
-                chown -R $user:$user $dir.local/share/qBittorrent/BT_backup/
-                rm -rf $dir.local/share/qBittorrent/logs/*
-                cp -R qBittorrent-Backup-Linux/logs/* $dir.local/share/qBittorrent/logs/
-                chown -R $user:$user $dir.local/share/qBittorrent/logs/
+            if [ "$is_flatpak" = true ]; then
+                rm -rf ${dirFlatpak}config/qBittorrent/*
+                cp -R qBittorrent-Backup-Linux/qBittorrent/* ${dirFlatpak}config/qBittorrent/
+                chown -R $user:$user ${dirFlatpak}config/qBittorrent/
+                rm -rf ${dirFlatpak}data/qBittorrent/BT_backup/*
+                cp -R qBittorrent-Backup-Linux/BT_backup/* ${dirFlatpak}data/qBittorrent/BT_backup/
+                chown -R $user:$user ${dirFlatpak}data/qBittorrent/BT_backup/
+                rm -rf ${dirFlatpak}data/qBittorrent/logs/*
+                cp -R qBittorrent-Backup-Linux/logs/* ${dirFlatpak}data/qBittorrent/logs/
+                chown -R $user:$user ${dirFlatpak}data/qBittorrent/logs/
             else
-                rm -rf $dir.local/share/data/qBittorrent/BT_backup/*
-                cp -R qBittorrent-Backup-Linux/BT_backup/* $dir.local/share/data/qBittorrent/BT_backup/
-                chown -R $user:$user $dir.local/share/data/qBittorrent/BT_backup/
-                rm -rf $dir.local/share/data/qBittorrent/logs/*
-                cp -R qBittorrent-Backup-Linux/logs/* $dir.local/share/data/qBittorrent/logs/
-                chown -R $user:$user $dir.local/share/data/qBittorrent/logs/
+                rm -rf ${dir}.config/qBittorrent/*
+                cp -R qBittorrent-Backup-Linux/qBittorrent/* ${dir}.config/qBittorrent/
+                chown -R $user:$user ${dir}.config/qBittorrent/
+                if [ -d ${dir}.local/share/qBittorrent/ ]; then
+                    rm -rf ${dir}.local/share/qBittorrent/BT_backup/*
+                    cp -R qBittorrent-Backup-Linux/BT_backup/* ${dir}.local/share/qBittorrent/BT_backup/
+                    chown -R $user:$user ${dir}.local/share/qBittorrent/BT_backup/
+                    rm -rf ${dir}.local/share/qBittorrent/logs/*
+                    cp -R qBittorrent-Backup-Linux/logs/* ${dir}.local/share/qBittorrent/logs/
+                    chown -R $user:$user ${dir}.local/share/qBittorrent/logs/
+                else
+                    rm -rf ${dir}.local/share/data/qBittorrent/BT_backup/*
+                    cp -R qBittorrent-Backup-Linux/BT_backup/* ${dir}.local/share/data/qBittorrent/BT_backup/
+                    chown -R $user:$user ${dir}.local/share/data/qBittorrent/BT_backup/
+                    rm -rf ${dir}.local/share/data/qBittorrent/logs/*
+                    cp -R qBittorrent-Backup-Linux/logs/* ${dir}.local/share/data/qBittorrent/logs/
+                    chown -R $user:$user ${dir}.local/share/data/qBittorrent/logs/
+                fi
             fi
 
-            # Clean
+            # Clean up the temporary backup directory
             rm -rf "$dir"qBittorrent-Backup-Linux/
             sleep 2
 
-            # End
+            # Displays the restore completion message
             clear
             echo -e "\n\033[1;32mRestoration completed.\033[0m"
             o1
